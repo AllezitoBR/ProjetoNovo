e4dcc3d9779bceb041036a9c5c5f4c760341b331
AllezitoBR <alle_andrade123@hotmail.com>
2024-11-12 21:04:41 -0300

    second commit

---
+++ type.py
@@ -0,0 +1,32 @@
+nome = "Eli"
+
+idade = 45
+
+altura = 1.65
+
+
+
+tipo_nome = type(nome)
+
+tipo_idade = type(idade)
+
+tipo_altura = type(altura)
+
+
+
+
+print("tipo de dado de 'nome':", tipo_nome)
+
+print("tipo de dado de 'idade':", tipo_idade)
+
+print("tipo de dado de 'altura':", tipo_altura)
+
+print(f"ola {tipo_altura}")
+
+name = input("Qual o seu nome: ")
+numero = int(input("Digite sua idade: "))
+name_type = type(name)
+numero_type =type(numero)
+
+print(name_type)
+print(numero_type)
